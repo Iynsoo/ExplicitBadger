ac9d866877bb76e1b35edcedfff8893431e2cfd4
wekwas <90058284+wekwas@users.noreply.github.com>
2022-04-25 11:34:03 -0500

    Implemented tests for MyUser

--- TA_Scheduling/tests.py
+++ TA_Scheduling/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import MyUser
+import unittest
 
-# Create your tests here.
+class TestMyUser(unittest.TestCase):
+    def test_NullUserName(self):
+        with self.assertRaises(Exception, msg="Username is null"):
+            m = MyUser(name=None, password="password")
+            m.full_clean()
+
+    def test_LongUserName(self):
+        with self.assertRaises(Exception, msg="Username is too long (>20)"):
+            m = MyUser(name="LoooooooooonoooogUsername", password="password")
+            m.full_clean()
+
+    def test_NullPassword(self):
+        with self.assertRaises(Exception, msg="Password is null"):
+            m = MyUser(name="Username", password=None)
+            m.full_clean()
+
+    def test_LongPassword(self):
+        with self.assertRaises(Exception, msg="Password is too long (>20)"):
+            m = MyUser(name="Username", password="LooooooooooooooongPassword")
+            m.full_clean()
+
+
