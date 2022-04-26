0ed9aa48e88791f481207979c30d68dcce79d218
iynsookim <iynsoo@uwm.edu>
2022-04-26 11:30:31 -0500

    modify the mdels.py and make new migration for this

--- TA_Scheduling/models.py
+++ TA_Scheduling/models.py
@@ -9,7 +9,7 @@ class Role(models.TextChoices):
 class MyUser(models.Model):
     name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    userType = models.CharField(max_length= 10, choices=Role.choices, default=Role.ta)
+    userType = models.CharField(max_length=10, choices=Role.choices, default=Role.ta)
     email = models.CharField(max_length=40)
-    first_name = models.CharField(max_length = 20)
-    last_name = models.CharField(max_length = 20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
