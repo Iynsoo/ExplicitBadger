c21c7422a6a2eddee21be444a6f29f8c910c2efd
Shend-uwm <70710119+Shend-uwm@users.noreply.github.com>
2022-05-17 15:02:52 -0700

    Added bootstrap to the html files. Edit account, change password files.

--- ExplicitBadger/urls.py
+++ ExplicitBadger/urls.py
@@ -16,17 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TA_Scheduling.views import *
-
+from TA_Scheduling.views import Edit_Account
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Login.as_view()),
-    path('home_admin/', Home_admin.as_view()),
-    path('createAccount/', Signup.as_view()),
-    path('deleteAccount/', Delete_Account.as_view()),
-    path('editAccount/', Edit_Account.as_view()),
-    path('createCourse/', Create_Course.as_view()),
-    path('createSection/', Create_Section.as_view()),
-    path('home_instructor/', Home_instructor.as_view()),
-    path('home_ta/', Home_ta.as_view()),
-    path('profile/', User_profile.as_view()),
+    path('', Login.as_view(), name='login'),
+    path('home_admin/', Home_admin.as_view(), name='home_admin'),
+    path('createAccount/', Signup.as_view(), name='create_account'),
+    path('deleteAccount/', Delete_Account.as_view(), name='delete_account'),
+    path('password/', PasswordsChangeView.as_view(template_name='change-password.html')),
+    path('password_success', password_sucess, name="password_success"),
+    path('editAccount/', Edit_Account.as_view(), name='edit_account'),
+    #path('deleteCourse/', Delete_Account.as_view(), name='delete_course'),
+    path('createCourse/', Create_Course.as_view(), name='create_course'),
+    path('viewCourse/', View_Course.as_view(), name='view_course'),
+    path('viewSection/', View_Section.as_view(), name='view_section'),
+    path('createSection/', Create_Section.as_view(), name='create_section'),
+    path('home_instructor/', Home_instructor.as_view(), name='home_instructor'),
+    path('home_ta/', Home_ta.as_view(), name='home_ta'),
+    path('profile/', User_profile.as_view(), name='profile')
 ]
--- TA_Scheduling/views.py
+++ TA_Scheduling/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from .models import MyUser
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View, generic
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
+from django.urls import reverse_lazy
+from .models import MyUser, course
+from .forms import PasswordChangingForm, EditAccountForm
 
 class Login(View):
     def get(self,request):
@@ -15,14 +19,147 @@ class Login(View):
         except:
             noSuchUser = True
         if noSuchUser:
-            return render(request,"login.html",{"message":"login failed"})
+            return render(request, "login.html", {"message": "login failed"})
         elif badPassword:
-            return render(request,"login.html",{"message":"bad password"})
+            return render(request, "login.html", {"message": "bad password"})
         else:
             request.session["name"] = m.name
-            return redirect("/home/")
+            print(m.userType)
+            if m.userType == 'Admin':
+                return redirect("/home_admin/")
+            elif m.userType == 'Instructor':
+                return redirect("/home_instructor/")
+            elif m.userType == 'TA':
+                return redirect("/home_ta/")
+            else:
+                return render(request, "login.html", {"message": "do not have a role"})
 
-class Home(View):
-    def get(self,request):
-        return render(request,"home.html",{})
+class Home_admin(View):
+    def get(self, request):
+        m = request.session["name"]
+        role = MyUser.objects.get(name=m).userType
+        return render(request, "home_admin.html", {"name": m, "role": role})
     pass
+
+class Signup(View):
+    def get(self, request):
+        return render(request, "createAccount.html", {})
+
+    def post(self, request):
+        name = request.POST['username']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        userType = request.POST['select_UserType']
+        email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        print(name)
+        print(pass1)
+        print(userType)
+        print(email)
+        print(fname)
+        print(lname)
+
+        if pass1 != pass2:
+            return render(request, "createAccount.html", {"message": "Enter same password"})
+        else:
+            MyUser.objects.create(name=name,password=pass1,userType=userType,email=email,first_name=fname,last_name=lname)
+            return render(request, "createAccount.html", {})
+
+class Delete_Account(View):
+    def get(self,request):
+        pass
+
+
+class Edit_Account(generic.CreateView):
+    form_class = EditAccountForm
+    template_name = 'editAccount.html'
+    success_url = reverse_lazy('login')
+
+    def get_object(self):
+        return not self.request.user
+
+class Create_Course(View):
+    def get(self, request):
+        all_courses = course.objects.all
+        all_Ins = MyUser.objects.filter(userType="Instructor")
+        return render(request, "createCourse.html", {"all": all_courses,"all_Ins": all_Ins})
+    def post(self,request):
+        all_courses = course.objects.all
+        all_Ins = MyUser.objects.filter(userType="Instructor")
+        s = request.POST.get('course', '')
+
+        corName = request.POST['courseName']
+        corTime = request.POST['courseTime']
+        if request.POST['userID'] == '':
+            id = None
+            corInstructor = 'None'
+        else:
+            id = MyUser.objects.get(id=request.POST['userID'])
+            corInstructor = id.__str__()
+        secNum = request.POST['sectionNum']
+        check = course.objects.filter(courseName=corName, sectionNum=secNum)
+
+        print(request.POST)
+        print(corName)
+        print(corTime)
+        print(secNum)
+        print(corInstructor)
+        #if s != '':
+        if check.exists():
+            return render(request, "createCourse.html", {"all": all_courses,"all_Ins":all_Ins, "message": "Course already exist!"})
+        else:
+            course.objects.create(courseName=corName, meetingTime=corTime, courseInstructor=corInstructor, sectionNum=secNum).userID.add(id)
+            return render(request, "createCourse.html", {"all": all_courses,"all_Ins":all_Ins, "message": "course created"})
+
+class Create_Section(View):
+    def get(self, request):
+        return render(request, "createSection.html", {})
+    pass
+
+class View_Section(View):
+    def get(self, request):
+        return render(request, "viewSection.html", {})
+    pass
+
+class View_Course(View):
+    def get(self, request):
+        return render(request, "viewCourse.html", {})
+    pass
+
+class Home_instructor(View):
+    def get(self, request):
+        m = request.session["name"]
+        role = MyUser.objects.get(name=m).userType
+        return render(request, "home_instructor.html", {"name": m, "role": role})
+    pass
+
+class Home_ta(View):
+    def get(self, request):
+        m = request.session["name"]
+        role = MyUser.objects.get(name=m).userType
+        return render(request, "home_ta.html", {"name": m, "role": role})
+    pass
+
+class User_profile(View):
+    def get(self, request):
+        name = request.session["name"]
+        this = MyUser.objects.get(name=name)
+        userType = this.userType
+        email = this.email
+        first_name = this.first_name
+        last_name = this.last_name
+        return render(request, "profile.html", {"name":name,"role":userType,"email":email,"fName":first_name,"lName":last_name})
+    def post(self, request):
+        email = request.POST["email"]
+        name = request.session["name"]
+        this = MyUser.objects.filter(name=name)
+        this.update(email=email)
+        return redirect("/profile/")
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_success')
+
+def password_sucess(request):
+    return render(request, 'password_success.html', {})
